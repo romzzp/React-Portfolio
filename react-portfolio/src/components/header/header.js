eed8efc2eedfea3327e50eb199f7504ca06fc608
romzz.git <romzz.patel@gmail.com>
2021-11-18 11:09:09 -0500

    added conditional statement to render the selected page from the navbar in header

--- react-portfolio/src/components/header/header.js
+++ react-portfolio/src/components/header/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 import NavBar from '../navbar/navbar';
 import About from '../../pages/about/about';
@@ -9,10 +9,29 @@ import Resume from '../../pages/resume/resume';
 
 
 function Header() {
+    const [currentPage, setCurrentPage] = useState('About');
+    const pageChange = (page) => setCurrentPage(page);
+
+    const pageRender = () => {
+        if (currentPage === 'Projects') {
+            return <Projects />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />
+        }
+        return <About />;
+    };
 
     return (
         <div>
-
+            <div>
+                <h2>Welcome</h2>
+                <NavBar currentPage={currentPage} pageChange={pageChange} />
+            </div>
+            {pageRender()}
         </div>
     )
 }
