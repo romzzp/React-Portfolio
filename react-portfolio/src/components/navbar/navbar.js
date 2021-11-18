46ed6c179df27092822ecd3b5fb30464ecd19565
romzz.git <romzz.patel@gmail.com>
2021-11-18 10:27:02 -0500

    created basic navbar

--- react-portfolio/src/components/navbar/navbar.js
+++ react-portfolio/src/components/navbar/navbar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './navbar.css';
 
-function Navbar() {
+function Navbar(props) {
     return (
         <div>
-
+            <nav>
+                <ul>
+                    <a href="#"><li onClick={() => props.handlePageChange("About")}>
+                        About
+                    </li></a>
+                    <a href="#"><li onClick={() => props.handlePageChange("Projects")}>
+                        Projects
+                    </li></a>
+                    <a href="#"><li onClick={() => props.handlePageChange("Contact")}>
+                        Contact
+                    </li></a>
+                    <a href="#"><li onClick={() => props.handlePageChange("Resume")}>
+                        Resume
+                    </li></a>
+                </ul>
+            </nav >
         </div>
     )
 };
