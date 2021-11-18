566e70309be8ff0be4ea80a87cc247198b017ebc
romzz.git <romzz.patel@gmail.com>
2021-11-18 11:01:53 -0500

    added footer content

--- react-portfolio/src/components/footer/footer.js
+++ react-portfolio/src/components/footer/footer.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './footer.css';
 
@@ -6,7 +7,9 @@ function Footer() {
 
     return (
         <div>
-
+            <a href="https://www.linkedin.com/in/romal-patel-a40521118"><img src="./images/linkedin2.png" width="50"
+                height="50"></img></a>
+            <a href="https://github.com/romzzp"><img src="./images/github.png" width="50" height="50"></img></a>
         </div>
     )
 }
