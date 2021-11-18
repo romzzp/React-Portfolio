e916954d7a6f4668355702af92904cfd18204019
romzz.git <romzz.patel@gmail.com>
2021-11-18 10:45:27 -0500

    added basic about me section

--- react-portfolio/src/pages/about/about.js
+++ react-portfolio/src/pages/about/about.js
@@ -6,7 +6,13 @@ function About() {
 
     return (
         <div>
-
+            {/* <img src= /> */}
+            <div>
+                <h1>Hi, I'm Romal Patel</h1>
+                <p>I am on my way to become a full stack Web Developer, I am curently enrolled in Full Stack Coding Bootcamp at University of Toronto.
+                    Please checkout the Projects I have worked on above.
+                </p>
+            </div>
         </div>
     )
 }
