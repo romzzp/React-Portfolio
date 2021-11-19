b17bf291ff3890cc5d86d05a1ed74e4334dcb629
romzz.git <romzz.patel@gmail.com>
2021-11-19 07:05:42 -0500

    added skill list to the resume section

--- react-portfolio/src/pages/resume/resume.js
+++ react-portfolio/src/pages/resume/resume.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './resume.css';
+import images from '../../assets';
 
 
 function Resume() {
 
     return (
-        <div>
-
+        <div class="resume">
+            <img src={images.mine} alt="profile pic" />
+            <div class="resumeLi">
+                <h1>The Skills I have:</h1>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Jquery</li>
+                <li>NodeJS</li>
+                <li>Express.JS</li>
+                <li>Handlebars.JS</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+                <li>Sequelize</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </div>
         </div>
     )
 }
